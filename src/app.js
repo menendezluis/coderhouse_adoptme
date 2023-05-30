3a64c248e8a617feaba64bce836c21884b4f23fb
Luis Menendez <luismenendez@users-MacBook-Pro.local>
2023-05-30 16:50:01 -0600

    Variable de entorno MONGO_URL

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
-import usersRouter from './routes/users.router.js';
-import petsRouter from './routes/pets.router.js';
-import adoptionsRouter from './routes/adoption.router.js';
-import sessionsRouter from './routes/sessions.router.js';
+import usersRouter from "./routes/users.router.js";
+import petsRouter from "./routes/pets.router.js";
+import adoptionsRouter from "./routes/adoption.router.js";
+import sessionsRouter from "./routes/sessions.router.js";
+import * as dotenv from "dotenv";
 
 const app = express();
-const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+dotenv.config();
+const PORT = process.env.PORT || 8080;
+const MONGO_URL = process.env.MONGO_URL;
+const connection = mongoose.connect(MONGO_URL);
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/pets", petsRouter);
+app.use("/api/adoptions", adoptionsRouter);
+app.use("/api/sessions", sessionsRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
